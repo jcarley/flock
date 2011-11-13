457b0c47b36bdbd5e73fac623f27f6e4c2b02ec7
Jefferson Carley <jeff.carley@gmail.com>
2011-11-13 14:36:33 -0600

    Added week of module

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,20 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    @week_of = this_week
+    @week_of = ::WeekOf.this_week_s
   end
-
-  private
-
-  def this_week
-    week_of
-  end
-
-  def week_of(week = 0)
-    day_offset = week * 7
-    today = Date.today.advance :days => day_offset
-    beginning_of_week = today.sunday? ? today : today.beginning_of_week.advance( :days => -1 )
-    end_of_week = beginning_of_week.advance :days => 6
-    "#{beginning_of_week.to_formatted_s :short} - #{end_of_week.to_formatted_s :short}"
-  end
-
 end
---
+++ lib/week_of.rb
@@ -0,0 +1,15 @@
+module WeekOf
+
+  def WeekOf.this_week_s
+     the_week = week_of
+     "#{the_week[:beginning].to_formatted_s :short} - #{the_week[:end].to_formatted_s :short}"
+  end
+
+  def WeekOf.week_of(week = 0)
+      day_offset = week * 7
+      today = Date.today.advance :days => day_offset
+      beginning_of_week = today.sunday? ? today : today.beginning_of_week.advance( :days => -1 )
+      end_of_week = beginning_of_week.advance :days => 6
+      week = { :beginning => beginning_of_week, :end => end_of_week }
+    end
+end
