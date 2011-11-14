3d4a622ae048aa1ccb1af3f1fae46440a2df8c76
Jefferson Carley <jeff.carley@gmail.com>
2011-11-14 07:20:52 -0600

    Wrote tests for week of module

--- lib/week_of.rb
+++ lib/week_of.rb
@@ -5,9 +5,10 @@ module WeekOf
      "#{the_week[:beginning].to_formatted_s :short} - #{the_week[:end].to_formatted_s :short}"
   end
 
-  def WeekOf.week_of(week = 0)
+  def WeekOf.week_of(week = 0, date = nil)
       day_offset = week * 7
-      today = Date.today.advance :days => day_offset
+      today = date.nil? ? Date.today : date
+      today = today.advance :days => day_offset
       beginning_of_week = today.sunday? ? today : today.beginning_of_week.advance( :days => -1 )
       end_of_week = beginning_of_week.advance :days => 6
       week = { :beginning => beginning_of_week, :end => end_of_week }
--- spec/modules/week_of_spec.rb
+++ spec/modules/week_of_spec.rb
@@ -4,12 +4,22 @@ describe WeekOf do
 
   describe "week_of" do
 
-    it "returns a hash for the beginning and end of the week" do
+    it "returns the proper beginning and end of the week" do
+      week = WeekOf.week_of 0, Date.new(2011, 11, 9)
+      week[:beginning].should == Date.new(2011, 11, 6)
+      week[:end].should == Date.new(2011, 11, 12)
+    end
 
-      week = WeekOf.week_of
-      week[:beginning].sunday?.should be_true
-      week[:end].saturday?.should be_true
+    it "returns the proper beginning and end of next week" do
+      week = WeekOf.week_of 1, Date.new(2011, 11, 9)
+      week[:beginning].should == Date.new(2011, 11, 13)
+      week[:end].should == Date.new(2011, 11, 19)
+    end
 
+    it "returns the proper beginning and end of previous week" do
+      week = WeekOf.week_of -1, Date.new(2011, 11, 9)
+      week[:beginning].should == Date.new(2011, 10, 30)
+      week[:end].should == Date.new(2011, 11, 5)
     end
 
   end
