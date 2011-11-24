0bc2f10a722ed010ad4f0c7409520bc727f8d9fe
Jefferson Carley <jeff.carley@gmail.com>
2011-11-24 08:10:39 -0600

    Added the admin field to the user's table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111122125048) do
+ActiveRecord::Schema.define(:version => 20111124140819) do
 
   create_table "timecards", :force => true do |t|
     t.integer  "user_id"
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(:version => 20111122125048) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",   :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20111122125048) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                                 :default => true
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
