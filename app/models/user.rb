8eebb9787416b0e74bfaa44e6ef9af3a5e55d002
Jefferson Carley <jeff.carley@gmail.com>
2011-12-03 20:33:17 -0600

    Sorts a user's timecard by volunteer_date

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
 
-  has_many :timecards
+  has_many :timecards, :order => 'volunteer_date'
 
 end
 
