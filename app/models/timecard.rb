b266d937e434b4f53a2ee5318013143241f72dcd
Jefferson Carley <jeff.carley@gmail.com>
2011-12-06 21:43:19 -0600

    Added class properties to work with formatted times.

--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -12,6 +12,31 @@ class Timecard < ActiveRecord::Base
     self.volunteer_date.to_formatted_s( :short_ordinal ) unless self.volunteer_date.nil?
   end
 
+  def start_time_string
+    start_time.strftime("%I:%M %P")
+  end
+
+  def start_time_string=(start_time_str)
+    self.start_time = Time.parse(start_time_str)
+  rescue ArgumentError
+    @start_time_invalid = true
+  end
+
+  def end_time_string
+    end_time.strftime("%I:%M %P")
+  end
+
+  def end_time_string=(end_time_str)
+    self.end_time = Time.parse(end_time_str)
+  rescue ArgumentError
+    @end_time_invalid = true
+  end
+
+  def validate
+    errors.add(:start_time, "is invalid") if @start_time_invalid
+    errors.add(:end_time, "is invalid") if @end_time_invalid
+  end
+
 end
 
 
